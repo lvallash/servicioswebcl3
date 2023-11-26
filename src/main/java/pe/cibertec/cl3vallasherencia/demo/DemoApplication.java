41e4a25d40446887bfd347569f9f664847ed4ab5
lvallash <leticiavh_3@hotmail.com>
2023-11-25 21:26:41 -0500

    base

---
+++ src/main/java/pe/cibertec/cl3vallasherencia/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.cl3vallasherencia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
